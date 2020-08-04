e719f0c1daed9d9d6377b69530c59b7c3833842d
hoangviethung <hoangviet.hung.009@gmail.com>
2020-08-04 17:17:42 +0700

    DONE ALL SECTION

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 import { getSVGs, Loading } from "./utilities/util";
 import Fullpage from "./libraries/Fullpage";
-
 // INIT FULLPAGE
 const initFullpage = () => {
 	// create instance fullpage
@@ -36,6 +35,50 @@ const setBackgroundImageSection = () => {
 	})
 }
 
+const initAnimatServiceHomePage = () => {
+	anime({
+		targets: '.service__item',
+		translateX: function (el) {
+			return el.getAttribute('data-x');
+		},
+		translateY: function (el, i) {
+			return 50 + (-50 * i);
+		},
+		scale: function (el, i, l) {
+			return (l - i) + .25;
+		},
+		rotate: function () { return anime.random(-360, 360); },
+		borderRadius: function () { return ['50%', anime.random(10, 35) + '%']; },
+		duration: function () { return anime.random(1200, 1800); },
+		delay: function () { return anime.random(0, 400); },
+		direction: 'alternate',
+		loop: true
+	})
+}
+
+// INIT SLIDER NEW
+const initSliderNews = () => {
+	const news = new Swiper('.slider-news .swiper-container', {
+		slidesPerView: 3,
+		spaceBetween: 27,
+		navigation: {
+			prevEl: '.slider-news-button-prev',
+			nextEl: '.slider-news-button-next',
+		},
+	})
+}
+
+// INIT SLIDER PRODUCTS
+const initSliderProducts = () => {
+	const products = new Swiper('.slider-products .swiper-container', {
+		slidesPerView: 4,
+		spaceBetween: 22,
+		navigation: {
+			prevEl: '.slider-products-button-prev',
+			nextEl: '.slider-products-button-next',
+		},
+	})
+}
 
 document.addEventListener("DOMContentLoaded", () => {
 	getSVGs();
@@ -44,4 +87,8 @@ document.addEventListener("DOMContentLoaded", () => {
 	initFullpage();
 	// SET BACKGROUND IMAGE SECTION
 	setBackgroundImageSection();
+	// INIT SLIDER NEWS
+	initSliderNews();
+	// INIT SLIDER PRODUCTS
+	initSliderProducts();
 });
